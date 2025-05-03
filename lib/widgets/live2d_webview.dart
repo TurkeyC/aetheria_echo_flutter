923574ee40e593f6b904a651a784a63739eb678f
Cao_Turkey_Su <u202413051@hust.edu.cn>
2025-05-03 14:49:19 +0800

    创建一个专门的WebView组件来加载Live2D

---
+++ lib/widgets/live2d_webview.dart
@@ -0,0 +1,82 @@
+// lib/widgets/live2d_webview.dart
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:convert';
+
+class Live2dWebView extends StatefulWidget {
+  final Function(String)? onMessageReceived;
+  final String initialUrl;
+
+  const Live2dWebView({
+    super.key,
+    this.onMessageReceived,
+    required this.initialUrl,
+  });
+
+  @override
+  State<Live2dWebView> createState() => _Live2dWebViewState();
+}
+
+class _Live2dWebViewState extends State<Live2dWebView> {
+  late final WebViewController controller;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(Colors.transparent)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (String url) {
+            setState(() {
+              isLoading = true;
+            });
+          },
+          onPageFinished: (String url) {
+            setState(() {
+              isLoading = false;
+            });
+          },
+          onWebResourceError: (WebResourceError error) {
+            debugPrint('WebView错误: ${error.description}');
+          },
+        ),
+      )
+      ..addJavaScriptChannel(
+        'FlutterApp',
+        onMessageReceived: (JavaScriptMessage message) {
+          if (widget.onMessageReceived != null) {
+            widget.onMessageReceived!(message.message);
+          }
+        },
+      )
+      ..loadRequest(Uri.parse(widget.initialUrl));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        WebViewWidget(controller: controller),
+        if (isLoading)
+          const Center(
+            child: CircularProgressIndicator(),
+          ),
+      ],
+    );
+  }
+
+  // 向Live2D发送消息的方法
+  Future<void> sendMessageToLive2d(String action, dynamic data) async {
+    final message = jsonEncode({
+      'action': action,
+      'data': data,
+    });
+
+    await controller.runJavaScript(
+      "window.receiveMsgFromFlutter($message)",
+    );
+  }
+}
